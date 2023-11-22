6c07982e472c8b8c94e96965386ae632493618a5
Gian Luca Decurtins <gldecurtins@icloud.com>
2023-11-22 20:46:34 +0100

    layout

--- container/app/main.py
+++ container/app/main.py
@@ -5,4 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/learn")
+def learn():
+    return render_template("learn.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
+@app.route("/certificate")
+def certificate():
+    return render_template("certificate.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
