224fa09e1bac52bdc1d3b7eebebf15bba588d76b
Gian Luca Decurtins <gldecurtins@icloud.com>
2023-11-27 18:24:00 +0100

    add question page

--- container/app/socialmediacert/test.py
+++ container/app/socialmediacert/test.py
@@ -10,6 +10,14 @@ bp = Blueprint("test", __name__, url_prefix="/test")
 def index():
     db = get_db()
     questions = db.execute(
-        f"SELECT id, question, option1, option2, option3, option4 FROM question"
+        "SELECT id, question, option1, option2, option3, option4 FROM question"
     ).fetchall()
     return render_template("test/index.html", questions=questions)
+
+@bp.route("/question/<int:id>")
+def question(id):
+    db = get_db()
+    question = db.execute(
+        f"SELECT id, question, option1, option2, option3, option4 FROM question WHERE id = {id}"
+    ).fetchone()
+    return render_template("test/question.html", question=question)
